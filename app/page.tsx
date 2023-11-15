7ea1ff6bd7fc96ae80c7544f07898e006bf4bd26
Maekelberg <maekelbergkjell@gmail.com>
2023-11-15 10:18:06 +0100

    reboot project

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,18 @@
 "use client";
 import Image from 'next/image'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
+import { FlowData, FormAnswer, FormQuestion, LinkToType } from './Data/Formdata';
+import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  const [videoActive, setVideoActive] = useState(true)
+  // Functionality hooks
+  const router = useRouter();
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {
-        videoActive ? "hello" : "nope"
-      } 
+    <main className={`flex min-h-screen flex-col items-start p-12`}>
+      <p className='font-bold text-lg mb-5'>Question 1: How many rams have you downloaded (select all that apply)?</p>
+      <article className='mb-3 p-5 border w-full'>Just 1</article>
+      <article className='mb-3 p-5 border w-full'>About 3</article>
     </main>
   )
 }
