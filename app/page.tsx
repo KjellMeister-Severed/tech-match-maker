95dc03cfd721be4b79a1f579900cd84c1e9d9333
Maekelberg <maekelbergkjell@gmail.com>
2023-11-15 11:51:59 +0100

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,38 @@
-"use client";
-import Image from 'next/image'
-import { useEffect, useState } from 'react'
-import { FlowData, FormAnswer, FormQuestion, LinkToType } from './Data/Formdata';
+'use client';
+import Image from 'next/image';
+import { useEffect, useState } from 'react';
+import {
+	FlowData,
+	FormAnswer,
+	FormQuestion,
+	LinkToType,
+} from './Data/Formdata';
 import { useRouter } from 'next/navigation';
 
 export default function Home() {
-  // Functionality hooks
-  const router = useRouter();
+	// Functionality hooks
+	const router = useRouter();
 
-  return (
-    <main className={`flex min-h-screen bg-formBgByLevel-1 flex-col items-start p-12`}>
-      <p className='font-bold text-lg mb-5'>Question 1: How many rams have you downloaded (select all that apply)?</p>
-      <article className='mb-3 p-5 border w-full'>Just 1</article>
-      <article className='mb-3 p-5 border w-full'>About 3</article>
-      <article className='mb-3 p-5 border w-full'>About 3</article>
-      <article className='mb-3 p-5 border w-full'>About 3</article>
-    </main>
-  )
+	return (
+		<main
+			className={`flex min-h-screen bg-black text-white flex-col items-start p-12`}>
+			<p className="font-bold text-lg mb-5">
+				Question 1: How many rams have you downloaded (select all that apply)?
+			</p>
+			<article className="mb-3 w-full h-20 rounded-3xl p-1 bg-gradient-to-r from-red-400 to-purple-400">
+				<div className="w-full px-4 h-full bg-slate-900 rounded-3xl flex items-center">
+					<p>Option 1: 1</p>
+				</div>
+			</article>
+			<article className="mb-3 w-full h-20 rounded-3xl p-1 bg-gradient-to-r from-red-400 to-purple-400">
+				<div className="w-full h-full bg-slate-900 rounded-3xl"></div>
+			</article>{' '}
+			<article className="mb-3 w-full h-20 rounded-3xl p-1 bg-gradient-to-r from-red-400 to-purple-400">
+				<div className="w-full h-full bg-slate-900 rounded-3xl"></div>
+			</article>{' '}
+			<article className="mb-3 w-full h-20 rounded-3xl p-1 bg-gradient-to-r from-red-400 to-purple-400">
+				<div className="w-full h-full bg-slate-900 rounded-3xl"></div>
+			</article>
+		</main>
+	);
 }
