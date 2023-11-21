01f5ddf4e8db66062a49bdcc3538144e504cd8e9
Maekelberg <maekelbergkjell@gmail.com>
2023-11-21 14:31:23 +0100

    finish flow

---
+++ app/api/chat-completion/route.ts
@@ -0,0 +1,21 @@
+import OpenAI from "openai";
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+
+export async function POST(req: Request) {
+  const {prompt} = (await req.json()) as {prompt: string};
+
+  if(!prompt) return new Response("Missing prompt", {status: 400});
+
+  const openai = new OpenAI({
+    apiKey: `${process.env.OPENAI_API_KEY}`
+  });
+
+  const openaires = await openai.chat.completions.create({
+    model: "gpt-3.5-turbo",
+    messages: [{"role": "user", "content": prompt}],
+    stream: true
+  });
+
+  const stream = OpenAIStream(openaires);
+  return new StreamingTextResponse(stream)
+}
